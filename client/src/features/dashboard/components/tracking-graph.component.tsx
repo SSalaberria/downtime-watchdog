585f90589d07f1297618fe2727b4453e730a54b1
Sebastian Salaberria <s_salaberria@hotmail.com>
2023-06-28 10:54:37 -0300

    client adaptations for tracked response times

--- client/src/features/dashboard/components/tracking-graph.component.tsx
+++ client/src/features/dashboard/components/tracking-graph.component.tsx
@@ -35,6 +35,9 @@ function getHourlyStatus(logs: Omit<TrackingLog, "tracker">[]) {
 const LogBar = memo(function LogBar({ logs }: { logs: Array<Omit<TrackingLog, "tracker">> }) {
   const data = useMemo(() => {
     const status = getHourlyStatus(logs);
+    const trackedLogs = logs.filter((log) => log.responseTime);
+    const responseTime =
+      trackedLogs.reduce((acc, log) => acc + (log?.responseTime || 0), 0) / trackedLogs.length;
 
     const createdAt =
       status === Status.Down
@@ -45,21 +48,41 @@ const LogBar = memo(function LogBar({ logs }: { logs: Array<Omit<TrackingLog, "t
       status,
       color: STATUS_COLORS[status],
       createdAt,
+      responseTime,
     };
   }, [logs]);
+
+  const formatResponseTime = (responseTime: number) => {
+    if (responseTime < 1000) {
+      return `${Math.floor(responseTime)} ms`;
+    }
+
+    return `${(responseTime / 1000).toFixed(1)} s`;
+  };
 
   return (
     <div className="group/item relative flex bg-green-200">
       <span
         className={`${data.color} h-0.5 w-2 transition-transform group-hover/item:scale-150 group-hover/item:bg-white`}
       />
-      <div className="invisible absolute left-4 z-10 w-36 rounded-2xl border border-accent-1 bg-background-primary p-2 group-hover/item:visible">
-        <div className={`mr-2 inline-block h-3 w-3 rounded-[50%] ${data.color}`} />
-        {`${Intl.DateTimeFormat(undefined, {
-          hour: "numeric",
-          minute: "numeric",
-          timeZone: "UTC",
-        }).format(new Date(data.createdAt))} UTC`}
+      <div className="invisible absolute left-4 z-50 inline-block rounded-2xl border border-accent-1 bg-background-primary p-2 group-hover/item:visible">
+        <div className=" min-w-max">
+          <div className={`mr-2 inline-block h-3 w-3 rounded-[50%] ${data.color}`} />
+          <p className="inline">
+            {`${Intl.DateTimeFormat(undefined, {
+              hour: "numeric",
+              minute: "numeric",
+              timeZone: "UTC",
+            }).format(new Date(data.createdAt))} UTC`}
+          </p>
+        </div>
+        <p className=" text-sm italic">
+          {data?.responseTime ? (
+            formatResponseTime(data.responseTime)
+          ) : (
+            <span className=" text-gray-600">Untracked</span>
+          )}
+        </p>
       </div>
     </div>
   );
